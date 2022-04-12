e0464115bde4b535de811dfb0cd778028b333d06
Alciomar Hollanda <alciomarhollanda@gmail.com>
2022-04-12 10:57:18 -0300

    Update test.sql

--- test.sql
+++ test.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.0.1
+-- version 5.1.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 09-Fev-2022 às 15:35
--- Versão do servidor: 10.4.6-MariaDB
--- versão do PHP: 7.3.9
+-- Tempo de geração: 12-Abr-2022 às 15:56
+-- Versão do servidor: 10.4.22-MariaDB
+-- versão do PHP: 8.1.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -21,6 +20,26 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `test`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `city`
+--
+
+CREATE TABLE `city` (
+  `idCity` int(11) NOT NULL,
+  `nameCity` varchar(300) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Extraindo dados da tabela `city`
+--
+
+INSERT INTO `city` (`idCity`, `nameCity`) VALUES
+(4, 'Hortolandia'),
+(6, 'Campinas'),
+(7, 'Sao gabriel da palha');
 
 -- --------------------------------------------------------
 
@@ -50,6 +69,12 @@ INSERT INTO `reg` (`name`, `username`, `password`, `city`, `image`, `gender`, `i
 --
 
 --
+-- Índices para tabela `city`
+--
+ALTER TABLE `city`
+  ADD PRIMARY KEY (`idCity`);
+
+--
 -- Índices para tabela `reg`
 --
 ALTER TABLE `reg`
@@ -58,6 +83,12 @@ ALTER TABLE `reg`
 --
 -- AUTO_INCREMENT de tabelas despejadas
 --
+
+--
+-- AUTO_INCREMENT de tabela `city`
+--
+ALTER TABLE `city`
+  MODIFY `idCity` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de tabela `reg`
